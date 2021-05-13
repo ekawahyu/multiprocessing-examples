0089834fb5f94815ae8034bb2d9c325e463fd1d8
Ekawahyu Susilo <ekawahyu@yahoo.com>
2021-05-12 21:14:36 -0700

    added print() in config module to show that it is executed with import *

--- multifiles-separate-processing/config.py
+++ multifiles-separate-processing/config.py
@@ -3,3 +3,5 @@ import multiprocessing
 # creating a lock object and multiprocessing variable
 lock = multiprocessing.Lock()
 balance = multiprocessing.Value('i', 100)
+
+print("This is configuration module")
